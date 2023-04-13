2cb4233de3aea37d71591fe6a4cddcc219b4959d
Abdujalol <73051897+tumbler-cp@users.noreply.github.com>
2023-04-13 16:58:36 +0300

    update

--- script.sql
+++ script.sql
@@ -1,7 +1,4 @@
 BEGIN;
-CREATE TYPE ship_actions as ENUM (
-    'находится в полёте'
-    );
 
 CREATE TYPE gender as ENUM (
     'М', 'Ж'
@@ -22,10 +19,7 @@ CREATE TABLE planets (
 
 CREATE TABLE ships (
     id SERIAL PRIMARY KEY,
-    name TEXT,
-    action ship_actions,
-    action_duration INT,
-    action_duration_unit duration_unit
+    name TEXT
 );
 
 
@@ -33,6 +27,11 @@ CREATE TABLE mini_worlds (
     id SERIAL PRIMARY KEY,
     name TEXT,
     is_isolated BOOLEAN
+);
+
+CREATE TABLE properties (
+    id SERIAL PRIMARY KEY,
+    feel_foreign BOOLEAN
 );
 
 CREATE TABLE humans (
@@ -43,7 +42,7 @@ CREATE TABLE humans (
     age INT,
     ship_id INT REFERENCES ships(id),
     mini_world_id INT REFERENCES mini_worlds(id),
-    property_id INT UNIQUE
+    property_id INT NOT NULL REFERENCES properties(id) UNIQUE
 );
 
 CREATE TABLE life_events (
@@ -53,19 +52,13 @@ CREATE TABLE life_events (
     feel_foreign BOOLEAN
 );
 
-CREATE TABLE properties (
-    id SERIAL PRIMARY KEY,
-    feel_foreign BOOLEAN
-);
-
-ALTER TABLE humans ADD CONSTRAINT f_property
-    FOREIGN KEY (property_id) REFERENCES properties(id);
-
 CREATE TABLE flights (
     id SERIAL PRIMARY KEY,
-    ship_id INT NOT NULL REFERENCES ships(id),
+    ship_id INT NOT NULL REFERENCES ships(id) UNIQUE,
     destination INT NOT NULL REFERENCES planets(id),
-    property_id INT NOT NULL REFERENCES properties(id)
+    property_id INT NOT NULL REFERENCES properties(id),
+    duration INT,
+    duration_unit duration_unit
 );
 
 CREATE TABLE years_of_studying (
@@ -84,20 +77,14 @@ CREATE TABLE institutions (
 CREATE TABLE semesters (
     year_of_study_id INT NOT NULL REFERENCES years_of_studying(id),
     institution_id INT NOT NULL REFERENCES institutions(id),
-    CONSTRAINT semester_id PRIMARY KEY (year_of_study_id, institution_id)
+    semester_id PRIMARY KEY (year_of_study_id, institution_id)
 );
 
 
 
-INSERT INTO ships(name, action, action_duration, action_duration_unit)
-VALUES (
-        'Корабль',
-        'находится в полёте',
-        30,
-        'день'
-       );
+INSERT INTO ships(name) VALUES ('Корабль'), ('Анон 1'), ('Анон 2');
 
-INSERT INTO planets(name) VALUES ('Луна'), ('Марс'), ('Земля');
+INSERT INTO planets(name) VALUES ('Луна'), ('Марс'), ('Земля'), ('Неизвестно');
 
 INSERT INTO properties(feel_foreign) VALUES (true);
 
@@ -114,8 +101,8 @@ VALUES (2, 'МТУ'), (3, 'ИТМО');
 INSERT INTO semesters(year_of_study_id, institution_id)
 VALUES (1, 1), (1, 2), (2, 2), (2, 1);
 
-INSERT INTO flights (ship_id, destination, property_id)
-VALUES (1, 1, 1), (1, 2, 1);
+INSERT INTO flights (ship_id, destination, property_id, duration, duration_unit)
+VALUES (2, 1, 1, null, null), (3, 2, 1, null, null), (1, 4, 1, 30, 'день');
 
 INSERT INTO humans(name, surname, gender, age, ship_id, mini_world_id, property_id)
 VALUES (
